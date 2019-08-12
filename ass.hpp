68e81147710e62bac0e693fa6a8dbc41d5049b5f
Mike Cowan <mike.c.cowan@gmail.com>
2019-08-12 14:06:41 +0100

    Add connect method

--- ass.hpp
+++ ass.hpp
@@ -12,3 +12,91 @@
  */
 
 #pragma once
+
+template<typename... Args>
+class Signal;
+
+template<typename... Args>
+class Slot final {
+
+    friend class Signal<Args...>;
+
+public:
+
+    explicit Slot(std::function<void(Args...)> callback)
+            : callback(std::move(callback)) {}
+
+    /**
+     * Returns the number of connections for this Slot.
+     *
+     * @return Number of connections for this Slot.
+     */
+    int connectionCount() const {
+        return signals.size();
+    }
+
+    /**
+     * Returns true if this Slot is connected to the provided Signal.
+     *
+     * @param signal Signal to test connection against.
+     * @return true if connected.
+     */
+    bool isConnectedTo(const Signal<Args...> &signal) const {
+        return std::find(signals.begin(), signals.end(), &signal) != signals.end();
+    }
+
+private:
+
+    void connectTo(Signal<Args...> &signal) {
+        signals.push_back(&signal);
+    }
+
+private:
+
+    std::function<void(Args...)> callback;
+
+    std::vector<Signal<Args...> *> signals;
+
+};
+
+template<typename... Args>
+class Signal final {
+
+public:
+
+    Signal() = default;
+
+    /**
+     * Connects this Signal to the provided Slot.
+     *
+     * @param slot Slot to connect this Signal to.
+     */
+    void connect(Slot<Args...> &slot) {
+        slots.push_back(&slot);
+        slot.connectTo(*this);
+    }
+
+    /**
+     * Returns the number of connections for this Signal.
+     *
+     * @return Number of connections for this Signal.
+     */
+    int connectionCount() const {
+        return slots.size();
+    }
+
+    /**
+     * Returns true if this Signal is connected to the provided Slot.
+     *
+     * @param slot Slot to test connection against.
+     * @return true if connected.
+     */
+    bool isConnectedTo(const Slot<Args...> &slot) const {
+        return std::find(slots.begin(), slots.end(), &slot) != slots.end();
+    }
+
+private:
+
+    std::vector<Slot<Args...> *> slots;
+
+};
